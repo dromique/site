40e846d0e2aa210c13e563ee1d0605bb0085dfe9
Dominique <538263@student.fontys.nl>
2026-09-07 11:23:33 +0200

    fixed another typo

--- app/flightOfTheSilverbirdS4/page.tsx
+++ app/flightOfTheSilverbirdS4/page.tsx
@@ -6,7 +6,7 @@ export default function CvPage() {
 			<div className="mx-auto flex w-full max-w-6xl flex-col gap-4">
 				<div className="overflow-hidden rounded-xl border border-black/10 bg-white shadow-lg">
 					{/* Embed video in page */}
-					<iframe className="h-[72svh] w-full sm:h-[78svh] md:h-[85vh]" src="https://www.youtube.com/embed/WeYXY4l22ZU?si=SCfrrai8Bh-raFgq" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+					<iframe className="h-[72svh] w-full sm:h-[78svh] md:h-[85vh]" src="https://www.youtube.com/embed/WeYXY4l22ZU?si=SCfrrai8Bh-raFgq" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
 				</div>
 			</div>
 		</main>
